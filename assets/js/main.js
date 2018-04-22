3426286351f9acb3419b3895e024afa0c70b2cf9
EscapeManOZK <romowczarek@gmail.com>
2018-04-22 23:32:52 +0200

    image

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,17 @@ $(document).ready(function(){
         }
     );
 });
+function $_GET(param) {
+    var vars = {};
+    window.location.href.replace( location.hash, '' ).replace(
+        /[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+        function( m, key, value ) { // callback
+            vars[key] = value !== undefined ? value : '';
+        }
+    );
+
+    if ( param ) {
+        return vars[param] ? vars[param] : null;
+    }
+    return vars;
+}²
